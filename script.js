fe6c18bf4af25364586c1e9f5eeacef931a9b008
chrisrhoden <carhoden@gmail.com>
2012-05-26 18:34:35 -0400

    colors

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 (function ($) {
-  var setPageFromHash = function() {
+  var setPageFromHash = function(e) {
     if (window.location.hash == '') return false;
+    if (e) { e.preventDefault(); }
     var activeFragment = $(window.location.hash);
     $(".revealer").not(activeFragment).fadeOut();
     activeFragment.fadeIn();
   }
 
-  var setWindowHeight = function () {
-    var $window = $(window);
-
-    $window.add('body').css('min-height', $window.height() - 50);
-  }
-
   $(function () {
     $('.revealer').hide();
 
-    setWindowHeight();
-
     $(window)
       .hashchange(setPageFromHash)
-      .resize(setWindowHeight)
     ;
 
     if (window.location.hash == '') {
